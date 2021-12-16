605b5c1a252d763107af8873253519d8560a6107
artilate <aleckrsek@gmail.com>
2021-12-15 18:41:40 -0800

    install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cp ./.config/alacritty/* $HOME/.config/alacritty/*
+cp ./.config/bspwm/* $HOME/.config/bspwm/*
+cp ./.config/dunst/* $HOME/.config/dunst/*
+cp ./.config/nvim/* $HOME/.config/nvim/*
+cp ./.config/polybar/* $HOME/.config/polybar/*
+cp ./.config/rofi/* $HOME/.config/rofi/*
+cp ./.config/sxhkd/* $HOME/.config/sxhkd/*
+
+
+
