b9d3247cc6afb587f7c83b529f5b4eaf2067590a
artilate <aleckrsek@gmail.com>
2021-12-16 21:24:11 -0800

    dotfile script

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
