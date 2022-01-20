3a0c00437486be9ee5111d589764fc316a1bf704
artilate <aleckrsek@gmail.com>
2022-01-19 22:32:44 -0800

    update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,9 +2,9 @@
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload left &
+    MONITOR=$m polybar --reload example &
   done
 else
-  polybar --reload left &
+  polybar --reload example &
 fi
 
